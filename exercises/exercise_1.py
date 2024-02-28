583901e1d51eb97d4e7d6a9389809c9ea668e85c
Solomon-99 <161601265+Solomon-99@users.noreply.github.com>
2024-02-28 13:05:51 +0000

    Exercise 1 solved

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,11 @@
 # Exercise 1
 # Your solution comes here
+
+number = int(input('Enter a 5 digit number: '))
+number = list(str(number))
+# print(number)
+
+new_Number = str(int(number[0]) + int(number[2]) + int(number[4]))
+new_Number = new_Number + str(int(number[1]) + int(number[3]))
+
+print(new_Number)
