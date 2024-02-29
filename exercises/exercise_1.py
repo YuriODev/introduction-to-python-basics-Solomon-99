431446dde4a031d5c9cad2de8da271accc779917
Solomon-99 <161601265+Solomon-99@users.noreply.github.com>
2024-02-29 07:41:13 +0000

    Exercise 1 and 2 again

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -11,10 +11,10 @@
 number = int(input('Enter a 5 digit number: '))
 
 num1 = number // 10000
-num2 = (number // 100) % 10
-num3 = number % 10
-num4 = (number // 1000) % 10
-num5 = (number // 10) % 10
+num2 = ((number // 1000) % 10)
+num3 = ((number // 100) % 10)
+num4 = ((number // 10) % 10)
+num5 = (number % 10)
 
 first_Part = num1 + num3 + num5
 second_Part = num2 + num4
--- exercises/exercise_2.py
+++ exercises/exercise_2.py
@@ -1,7 +1,7 @@
 # Exercise 2
 # Your solution comes here
 
-n = int(input('Enter an integer: '))
+n = int(input('Enter an integer:'))
 if n % 2 == 0:
     print(n + 2)
 else:
